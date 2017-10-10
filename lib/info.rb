0339f798581b57319f632b86e82d5bc93e5d209f
github username <12ivazquez@gmail.com>
2017-10-10 12:44:32 -0500

    Mangeable list view w/ more user prompts & flow

--- lib/info.rb
+++ lib/info.rb
@@ -1,23 +1,37 @@
 require_relative "../lib/scraper.rb"
 
-def list_options
+def make_options
   Scraper.make_sites
-  Site.all.each.with_index(1) do |site, i|
+end
+
+def list_options(start_number)
+  puts "---------- UNESCO Heritage Sites #{start_number} - #{start_number+99} ----------"
+  Site.all[start_number-1, 100].each.with_index(start_number) do |site, i|
     puts "#{i}. #{site.title}"
   end
 end
 
 def menu
-  puts "Enter the site number you would like more info on or type exit:"
+  puts "What sites would you like to see? 1-100, 101-200, 201-300, ... 1001-1100"
+  input = gets.strip.to_i
+  list_options(input)
+  puts "Type the site number you would like more info on or exit:"
   input = nil
   while input != "exit"
-    input = gets.strip
-    if input.to_i > 0
+    input = gets.strip.downcase
+    if input.to_i > 0 && input.to_i <= Site.all.count
       site = Site.all[input.to_i - 1]
       puts "#{site.title}"
+      puts ""
       puts "#{site.description}"
+      puts ""
+      puts "Type list to see the options again or exit."
+    elsif input == "list"
+      menu
     else
-      puts "Type an integer or exit please."
+      break if input == "exit"
+      puts "Type a valid integer or exit."
     end
   end
+  puts "Goodbye and happy learning!"
 end
--- spec.md
+++ spec.md
@@ -2,6 +2,8 @@
 
 Specs:
 - [x] Have a CLI for interfacing with the application
-- [ ] Pull data from an external source
-- [ ] Implement both list and detail views
-
+  prompts user with directions on how to look up info
+- [x] Pull data from an external source
+  created objects immediately placed in an array
+- [x] Implement both list and detail views
+  user can see a manageable list of 100 items and then pick a description to read
